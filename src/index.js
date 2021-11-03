309ccb8a798030e7392a4a55d3d894ee969ae1c3
Karen <karen.zoroglyan@mail.ru>
2021-11-03 18:25:04 +0400

    Добавил правила для eslint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-import "./style.css";
+import './style.css'
+
+/**
+ * Добавить пре-коммит хук чтобы перед коммитом еслинт проверял код:
+ * если еслинт нашел ошибки в коде - коммит не пройдет
+ * если еслинт не нашел проблем в коде - коммит проходит
+ *
+ * https://github.com/typicode/husky
+ * https://github.com/typicode/husky#usage
+ * https://typicode.github.io/husky/#/
+ * 
+ * 
+ * 
+ * https://eslint.org/docs/rules/
+ * Добавить правила для еслинта
+ * 
+ * не должно быть неиспользуемых переменных
+ * всегда в конце файла должна быть пустая линия
+ * только конст и лет, без var
+ * no-debugger
+ * 	no-multiple-empty-lines
+ * 	no-unexpected-multiline
+ * и тд
+ */
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,39 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
-const PrettierPlugin = require("prettier-webpack-plugin");
+// task: как сделать так чтобы еслинт игнорил файл
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
             options: { minimize: false },
           },
         ],
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
+      template: './src/index.html',
+      filename: './index.html',
     }),
-    new ESLintPlugin(options),
-    new PrettierPlugin(),
   ],
   devServer: {
     compress: true,
     port: 3000,
   },
-};
+}
